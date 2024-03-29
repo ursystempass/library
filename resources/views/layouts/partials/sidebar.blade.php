505d3b2781883f98e03f9ffad98264e9f0b7b7ee
ursystempass <jungw0npepsi@gmail.com>
2024-03-29 14:57:51 +0700

    ft: sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,15 +2,15 @@
     <ul class="nav">
       <li class="nav-item nav-profile">
         <a href="#" class="nav-link">
-          <div class="nav-profile-image">
+          {{-- <div class="nav-profile-image">
             <img src="assets/images/faces/face1.jpg" alt="profile">
             <span class="login-status online"></span>
             <!--change to offline or busy as needed-->
-          </div>
-          <div class="nav-profile-text d-flex flex-column">
+          </div> --}}
+          {{-- <div class="nav-profile-text d-flex flex-column">
             <span class="font-weight-bold mb-2">Library Millenium</span>
             <span class="text-secondary text-small">Admin Perpustakaan</span>
-          </div>
+          </div> --}}
           <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
         </a>
       </li>
@@ -22,26 +22,41 @@
       </li>
       <li class="nav-item">
           <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <span class="menu-title">Inventaris</span>
+              <span class="menu-title">Anggota</span>
               <i class="menu-arrow"></i>
               <i class="mdi mdi-crosshairs-gps menu-icon"></i>
           </a>
           <div class="collapse" id="ui-basic">
-              {{-- <ul class="nav flex-column sub-menu">
+              <ul class="nav flex-column sub-menu">
+                  {{-- <li class="nav-item"> <a class="nav-link" href="{{ route('c') }}">Buku</a></li> --}}
+                  <li class="nav-item"> <a class="nav-link" href="{{ route('users.index') }}">Siswa</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="{{ route('classes.index') }}">Kelas</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="{{ route('majors.index') }}">Jurusan</a></li>
+              </ul>
+          </div>
+      </li>
+      {{-- <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <span class="menu-title">Anggota</span>
+              <i class="menu-arrow"></i>
+              <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+          </a>
+          <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
                   <li class="nav-item"> <a class="nav-link" href="{{ route('books.index') }}">Buku</a></li>
                   <li class="nav-item"> <a class="nav-link" href="{{ route('acquisitions.index') }}">Data Perolehan</a></li>
                   <li class="nav-item"> <a class="nav-link" href="{{ route('reductionbook.index') }}">Data Keluar</a></li>
                   <li class="nav-item"> <a class="nav-link" href="{{ route('shelves.index') }}">Rak Buku</a></li>
-              </ul> --}}
+              </ul>
           </div>
-      </li>
+      </li> --}}
 
-      <li class="nav-item">
+      {{-- <li class="nav-item">
           <a class="nav-link" href="{{ url('/users') }}">
               <span class="menu-title">Anggota</span>
               <i class="mdi mdi-home menu-icon"></i>
             </a>
-      </li>
+      </li> --}}
       <li class="nav-item">
           <a class="nav-link" href="{{ url('/borrowingsdetails') }}">
               <span class="menu-title">Detail Peminjaman</span>
@@ -55,7 +70,7 @@
             </a>
       </li>
 
-      <li class="nav-item">
+      {{-- <li class="nav-item">
           <a class="nav-link" href="{{ url('/majors') }}">
               <span class="menu-title">Jurusan</span>
               <i class="mdi mdi-home menu-icon"></i>
@@ -66,7 +81,7 @@
               <span class="menu-title">Kelas</span>
               <i class="mdi mdi-home menu-icon"></i>
             </a>
-      </li>
+      </li> --}}
 
       {{-- <li class="nav-item">
           <a class="nav-link" href="{{ url('/reduction-books') }}">
@@ -81,37 +96,37 @@
               <i class="mdi mdi-home menu-icon"></i>
             </a>
       </li>
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link" href="pages/icons/mdi.html">
           <span class="menu-title">Icons</span>
           <i class="mdi mdi-contacts menu-icon"></i>
         </a>
-      </li>
-      <li class="nav-item">
+      </li> --}}
+      {{-- <li class="nav-item">
         <a class="nav-link" href="pages/forms/basic_elements.html">
           <span class="menu-title">Forms</span>
           <i class="mdi mdi-format-list-bulleted menu-icon"></i>
         </a>
-      </li>
+      </li> --}}
       <li class="nav-item">
         <a class="nav-link" href="pages/charts/chartjs.html">
           <span class="menu-title">Charts</span>
           <i class="mdi mdi-chart-bar menu-icon"></i>
         </a>
       </li>
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link" href="pages/tables/basic-table.html">
           <span class="menu-title">Tables</span>
           <i class="mdi mdi-table-large menu-icon"></i>
         </a>
-      </li>
+      </li> --}}
       <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
+        {{-- <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
           <span class="menu-title">Sample Pages</span>
           <i class="menu-arrow"></i>
           <i class="mdi mdi-medical-bag menu-icon"></i>
-        </a>
-        <div class="collapse" id="general-pages">
+        </a> --}}
+        {{-- <div class="collapse" id="general-pages">
           <ul class="nav flex-column sub-menu">
             <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
             <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
@@ -119,11 +134,11 @@
             <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
             <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
           </ul>
-        </div>
+        </div> --}}
       </li>
       <li class="nav-item sidebar-actions">
         <span class="nav-link">
-          <div class="border-bottom">
+          {{-- <div class="border-bottom">
             <h6 class="font-weight-normal mb-3">Projects</h6>
           </div>
           <button class="btn btn-block btn-lg btn-gradient-primary mt-4">+ Add a project</button>
@@ -135,7 +150,7 @@
               <li>Free</li>
               <li>Pro</li>
             </ul>
-          </div>
+          </div> --}}
           <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}"
                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
