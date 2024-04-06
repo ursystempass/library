2579c45e71636f895626df5fadd02494265b375b
ursystempass <jungw0npepsi@gmail.com>
2024-04-06 23:55:27 +0700

    fr: crud reback -edit

---
+++ app/Models/ReturnBack.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReturnBack extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function return_details()
+    {
+        return $this->hasMany(ReturnDetail::class);
+    }
+}
