b85f98737533e4d7959ef64c4ead240c715fc741
ursystempass <jungw0npepsi@gmail.com>
2024-04-21 00:14:50 +0700

    ft: rand i push

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Setting;
 use App\Models\Type; // Import model Type
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -12,8 +13,9 @@ class CatalogController extends Controller
     public function index()
     {
         $books = Book::all(); // Mengambil semua data buku dari database
+        $setting = Setting::all(); // Mengambil semua data buku dari database
         $types = Type::all(); // Mengambil semua data jenis buku dari database
-        return view('member.catalog', compact('books', 'types')); // Mengirim data buku dan jenis buku ke tampilan
+        return view('member.catalog', compact('books', 'types', 'setting')); // Mengirim data buku dan jenis buku ke tampilan
     }
 
     public function greeting()
